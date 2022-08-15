011a5cdfe28be4f6b8bada4a71deaba264cd5e12
Lukas Kral <lukywill16@gmail.com>
2022-08-15 14:00:11 +0200

    fix loading of document
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- theme.js
+++ theme.js
@@ -1,1 +1,3 @@
-document.getElementById("jenkins-head-icon").src="https://www.jenkins.io/images/logos/snow/snow.png";
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("jenkins-head-icon").src="https://www.jenkins.io/images/logos/snow/snow.png";
+});
