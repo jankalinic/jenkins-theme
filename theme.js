22b5359db34e2fbac32e8b10f3ae7283ba9af9b9
Lukas Kral <lukywill16@gmail.com>
2022-08-15 14:33:51 +0200

    hmm nice
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- theme.js
+++ theme.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById("jenkins-head-icon").src="https://www.jenkins.io/images/logos/snow/snow.png";
+    var a = new Audio("http://soundfxcenter.com/movies/star-wars/8d82b5_Star_Wars_Main_Theme_Song.mp3");
+    a.play();
 });
